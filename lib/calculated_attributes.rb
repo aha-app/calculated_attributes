e6a0dc01a05d1f3ee468ce2283702d7f2adb74ef
Zach Schneider <schneidmaster@gmail.com>
2015-06-24 10:34:57 -0500

    Don't attempt to define calculated attributes * Causes odd exceptions under certain conditions

--- lib/calculated_attributes.rb
+++ lib/calculated_attributes.rb
@@ -94,9 +94,76 @@ module ActiveRecord
       apply_join_dependency(relation, join_dependency)
     end
   end
-end
 
-module ActiveRecord
+  module AttributeMethods
+    module ClassMethods
+      # Generates all the attribute related methods for columns in the database
+      # accessors, mutators and query methods.
+      def define_attribute_methods
+        case ActiveRecord::VERSION::MAJOR
+        when 3
+          unless defined?(@attribute_methods_mutex)
+            msg = "It looks like something (probably a gem/plugin) is overriding the " \
+                  "ActiveRecord::Base.inherited method. It is important that this hook executes so " \
+                  "that your models are set up correctly. A workaround has been added to stop this " \
+                  "causing an error in 3.2, but future versions will simply not work if the hook is " \
+                  "overridden. If you are using Kaminari, please upgrade as it is known to have had " \
+                  "this problem.\n\n"
+            msg << "The following may help track down the problem:"
+
+            meth = method(:inherited)
+            if meth.respond_to?(:source_location)
+              msg << " #{meth.source_location.inspect}"
+            else
+              msg << " #{meth.inspect}"
+            end
+            msg << "\n\n"
+
+            ActiveSupport::Deprecation.warn(msg)
+
+            @attribute_methods_mutex = Mutex.new
+          end
+
+          # Use a mutex; we don't want two thread simaltaneously trying to define
+          # attribute methods.
+          @attribute_methods_mutex.synchronize do
+            return if attribute_methods_generated?
+            superclass.define_attribute_methods unless self == base_class
+            columns_to_define =
+              if defined?(calculated) && calculated.instance_variable_get('@calculations')
+                calculated_keys = calculated.instance_variable_get('@calculations').keys
+                column_names.reject { |c| calculated_keys.include? c.intern }
+              else
+                column_names
+              end
+            super(columns_to_define)
+            columns_to_define.each { |name| define_external_attribute_method(name) }
+            @attribute_methods_generated = true
+          end
+
+        when 4
+          return false if @attribute_methods_generated
+          # Use a mutex; we don't want two threads simultaneously trying to define
+          # attribute methods.
+          generated_attribute_methods.synchronize do
+            return false if @attribute_methods_generated
+            superclass.define_attribute_methods unless self == base_class
+            columns_to_define =
+              if defined?(calculated) && calculated.instance_variable_get('@calculations')
+                calculated_keys = calculated.instance_variable_get('@calculations').keys
+                column_names.reject { |c| calculated_keys.include? c.intern }
+              else
+                column_names
+              end
+            super(columns_to_define)
+            @attribute_methods_generated = true
+          end
+          true
+        end
+      end
+    end
+  end
+
   module Associations
     class JoinDependency
       attr_writer :calculated_columns
