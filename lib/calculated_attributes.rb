9f58cca1b755a813f07f0440631f4e3ab093c553
Zach Schneider <schneidmaster@gmail.com>
2015-07-01 11:05:18 -0500

    Fix for Rails 4.2

--- lib/calculated_attributes.rb
+++ lib/calculated_attributes.rb
@@ -193,12 +193,16 @@ module ActiveRecord
       # Updates the attribute identified by <tt>attr_name</tt> with the specified +value+. Empty strings
       # for fixnum and float columns are turned into +nil+.
       def write_attribute(attr_name, value)
-        attr_name = attr_name.to_s
-        attr_name = self.class.primary_key if attr_name == 'id' && self.class.primary_key
-        @attributes_cache.delete(attr_name)
-        column = column_for_attribute(attr_name)
+        if ActiveRecord::VERSION::MAJOR == 4 && ActiveRecord::VERSION::MINOR == 2
+          write_attribute_with_type_cast(attr_name, value, true)
+        else
+          attr_name = attr_name.to_s
+          attr_name = self.class.primary_key if attr_name == 'id' && self.class.primary_key
+          @attributes_cache.delete(attr_name)
+          column = column_for_attribute(attr_name)
 
-        @attributes[attr_name] = type_cast_attribute_for_write(column, value)
+          @attributes[attr_name] = type_cast_attribute_for_write(column, value)
+        end
       end
     end
   end
