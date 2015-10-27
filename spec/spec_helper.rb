a7c46cf62f15f16109818a2ad51ae5222fa143a1
Zach Schneider <zach@schneid.io>
2015-10-27 15:14:20 -0500

    Refactor rails 3 and 4 patches into separate files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,4 @@ require 'calculated_attributes'
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3',
                                         database: File.dirname(__FILE__) + '/calculated_attributes.sqlite3')
 
-load File.dirname(__FILE__) + '/support/schema.rb'
-load File.dirname(__FILE__) + '/support/models.rb'
-load File.dirname(__FILE__) + '/support/data.rb'
+%w(schema models data).each { |f| load File.dirname(__FILE__) + "/support/#{f}.rb" }
