8174d84f9b8e8e88bb813860bcfb940ee9d55cc8
shane tully <shane@shanetully.com>
2025-03-20 18:50:54 -0700

    PFT-254: add rails 8 support

--- lib/calculated_attributes.rb
+++ lib/calculated_attributes.rb
@@ -5,9 +5,9 @@ require 'active_record'
 require 'calculated_attributes/rails_patches'
 require 'calculated_attributes/arel_patches'
 
-raise "Unsupported ActiveRecord version: #{ActiveRecord::VERSION::MAJOR}" unless [6, 7].include? ActiveRecord::VERSION::MAJOR
+raise "Unsupported ActiveRecord version: #{ActiveRecord::VERSION::MAJOR}" unless [7, 8].include? ActiveRecord::VERSION::MAJOR
 
-require "calculated_attributes/rails_#{ActiveRecord::VERSION::MAJOR}_patches"
+require "calculated_attributes/rails_#{ActiveRecord::VERSION::MAJOR}_#{ActiveRecord::VERSION::MINOR}_patches"
 
 # Include model code.
 require 'calculated_attributes/model_methods'
